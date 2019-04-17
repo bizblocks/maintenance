0578fd336f4b24757d4e83dcea6963bf621e49c8
gecrepo <gecrepo@gmail.com>
2019-04-17 19:33:18 +0400

    Do not check maintenance for user with expired session

--- modules/web/src/com/groupstp/maintenance/web/login/ExtAppLoginWindow.java
+++ modules/web/src/com/groupstp/maintenance/web/login/ExtAppLoginWindow.java
@@ -8,10 +8,16 @@ import com.haulmont.cuba.gui.components.ScrollBoxLayout;
 import com.haulmont.cuba.gui.executors.BackgroundWorker;
 import com.haulmont.cuba.gui.executors.UIAccessor;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
+import com.haulmont.cuba.security.auth.Credentials;
+import com.haulmont.cuba.security.global.LoginException;
 import com.haulmont.cuba.web.app.loginwindow.AppLoginWindow;
 import com.vaadin.server.*;
+import com.vaadin.ui.UIDetachedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import java.util.Map;
 
 /**
  * Extended application login window to support maintenance page
@@ -19,6 +25,7 @@ import javax.inject.Inject;
  * @author adiatullin
  */
 public class ExtAppLoginWindow extends AppLoginWindow {
+    private static final Logger log = LoggerFactory.getLogger(ExtAppLoginWindow.class);
 
     @Inject
     protected BackgroundWorker backgroundWorker;
@@ -37,38 +44,58 @@ public class ExtAppLoginWindow extends AppLoginWindow {
 
     protected UIAccessor uiAccessor;
 
+    protected RequestHandler requestHandler = (RequestHandler) (s, req, res) -> checkMaintenancePage();
+
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+
+        uiAccessor = backgroundWorker.getUIAccessor();
+    }
+
     @Override
     public void ready() {
         super.ready();
-
-        uiAccessor = backgroundWorker.getUIAccessor();
 
         setupMaintenanceRequestHandler();
         checkMaintenancePage();
     }
 
     protected void setupMaintenanceRequestHandler() {
-        VaadinSession.getCurrent().addRequestHandler((RequestHandler) (s, req, res) -> checkMaintenancePage());
+        VaadinSession.getCurrent().addRequestHandler(requestHandler);
     }
 
     protected boolean checkMaintenancePage() {
-        uiAccessor.accessSynchronously(() -> {
-            boolean showMaintenance = false;
-            if (Boolean.TRUE.equals(maintenanceConfig.getEnabled())) {
-                showMaintenance = !VaadinService.getCurrentRequest()
-                        .getParameterMap().containsKey(maintenanceConfig.getLoginParameterName());
-            }
-            if (showMaintenance) {
-                maintenanceBox.removeAll();
-                HtmlBoxLayout layout = componentsFactory.createComponent(HtmlBoxLayout.class);
-                layout.setTemplateContents(maintenanceConfig.getMaintenancePage());
-                layout.setWidth("100%");
-                maintenanceBox.add(layout);
-            }
-            maintenanceBox.setVisible(showMaintenance);
-            loginWrapper.setVisible(!showMaintenance);
-            poweredByLink.setVisible(!showMaintenance && webConfig.getLoginDialogPoweredByLinkVisible());
-        });
+        try {
+            uiAccessor.accessSynchronously(() -> {
+                boolean showMaintenance = false;
+                if (Boolean.TRUE.equals(maintenanceConfig.getEnabled())) {
+                    showMaintenance = !VaadinService.getCurrentRequest()
+                            .getParameterMap().containsKey(maintenanceConfig.getLoginParameterName());
+                }
+                if (showMaintenance) {
+                    maintenanceBox.removeAll();
+                    HtmlBoxLayout layout = componentsFactory.createComponent(HtmlBoxLayout.class);
+                    layout.setTemplateContents(maintenanceConfig.getMaintenancePage());
+                    layout.setWidth("100%");
+                    maintenanceBox.add(layout);
+                }
+                maintenanceBox.setVisible(showMaintenance);
+                loginWrapper.setVisible(!showMaintenance);
+                poweredByLink.setVisible(!showMaintenance && webConfig.getLoginDialogPoweredByLinkVisible());
+            });
+        } catch (UIDetachedException e) {
+            log.debug("Ignore to show maintenance page since user session already expired");
+            VaadinSession.getCurrent().removeRequestHandler(requestHandler);
+        }
         return false;
+    }
+
+    @Override
+    protected void doLogin(Credentials credentials) throws LoginException {
+        super.doLogin(credentials);
+
+        VaadinSession.getCurrent().removeRequestHandler(requestHandler);
     }
 }
